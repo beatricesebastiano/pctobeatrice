dfbdebb914388acd3312358168905c6f08c5a742
beatricesebastiano <124668921+beatricesebastiano@users.noreply.github.com>
2023-02-07 12:01:52 +0100

    Add files via upload

---
+++ numeri.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+ int a;
+ int b;
+ printf("inserisci due interi\n");
+ scanf("%d %d", &a, &b);
+ int divisione = a/b;
+ printf("risultato della divisione : %d\n", divisione);
+
+
+
+
+
+
+
+}
