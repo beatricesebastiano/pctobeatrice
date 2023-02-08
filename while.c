5afbd5d65da6611093e90705bc8dfe048ffb1c57
beatricesebastiano <124668921+beatricesebastiano@users.noreply.github.com>
2023-02-08 12:23:05 +0100

    Add files via upload

---
+++ while.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+int x= 5;
+int y= 10;
+
+while (x < y)
+{
+    printf("ao\n");
+    x = x + 1;
+}
+
+
+
+
+
+
+
+
+
+
+}
---
+++ while2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+
+int x=0;
+
+while (x <= 10)
+{
+printf("x %d\n",x);
+x = x + 1;
+}
+
+
+
+
+
+}
